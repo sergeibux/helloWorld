075195f6780bc696693c5a389fbed94d632ea827
sergeibux <smv@smvDeb.sertux>
2020-05-06 11:05:21 +0200

    Saying hello & know who is speaking to us

--- Bonjour.java
+++ Bonjour.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 class Bonjour{
 	public static void main(String[] args){
-		System.out.println("Bonjour le jour :)");
+		Scanner name = new Scanner(System.in);
+		System.out.println("Hi ! What's your name ? ");
+		String strName = name.nextLine();
+		System.out.println("Hello Mister " + strName + " :)");
 	}
 }
