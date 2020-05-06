b19b544bf244b984641265f58befaed10c1f981d
sergeibux <smv@smvDeb.sertux>
2020-05-06 11:37:02 +0200

    alternative if no args

--- BonjourPersonnalise.java
+++ BonjourPersonnalise.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 class BonjourPersonnalise{
 	public static void main(String[] args){
-		System.out.println("Hi ! What's your name ? ");
-		if (args.length < 1)
-			throw new RuntimeException("args < 1 : " + args.length);
-		String strName = args[0];
+		System.out.println("Hi ! ");
+		String strName = "";
+		if (args.length < 1){
+			Scanner name = new Scanner(System.in);
+			System.out.println("What's your name ? ");
+			strName = name.nextLine();
+		} else {
+			strName = args[0];
+		}
 		System.out.println("Hello Mister " + strName + " :)");
 	}
 }
