e4641d66f8df5dae80aeb57ecd0010032217b6b1
sergeibux <smv@smvDeb.sertux>
2020-05-06 11:39:49 +0200

    commented

--- BonjourPersonnalise.java
+++ BonjourPersonnalise.java
@@ -1,9 +1,18 @@
+/**
+ * @author Merliere Vanhoeke Sébastien
+ */
+
 import java.util.Scanner;
 
+/*
+ * We know what it does :)
+ * yes ! we know :D
+ */
 class BonjourPersonnalise{
 	public static void main(String[] args){
 		System.out.println("Hi ! ");
 		String strName = "";
+		// don't crash if no arg sended
 		if (args.length < 1){
 			Scanner name = new Scanner(System.in);
 			System.out.println("What's your name ? ");
