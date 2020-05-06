965a768cbbf603d0985a24323ef392326fa5238a
sergeibux <smv@smvDeb.sertux>
2020-05-06 11:16:58 +0200

    Saying hello & know who is speaking to us before running

---
+++ BonjourPersonnalise.java
@@ -0,0 +1,7 @@
+class BonjourPersonnalise{
+	public static void main(String[] args){
+		System.out.println("Hi ! What's your name ? ");
+		String strName = args[0];
+		System.out.println("Hello Mister " + strName + " :)");
+	}
+}
