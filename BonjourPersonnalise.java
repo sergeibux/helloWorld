8b2546be522e1af211331ee9e3277b8c3e633eac
sergeibux <smv@smvDeb.sertux>
2020-05-06 11:33:10 +0200

    catch error if no args

--- BonjourPersonnalise.java
+++ BonjourPersonnalise.java
@@ -1,6 +1,8 @@
 class BonjourPersonnalise{
 	public static void main(String[] args){
 		System.out.println("Hi ! What's your name ? ");
+		if (args.length < 1)
+			throw new RuntimeException("args < 1 : " + args.length);
 		String strName = args[0];
 		System.out.println("Hello Mister " + strName + " :)");
 	}
